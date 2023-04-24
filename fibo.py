7e172f3b0d5765c8cff451c0fd1b45574a0c6267
Dankova Barbora <247216@vutbr.cz>
2023-04-24 09:35:49 +0200

    1

--- fibo.py
+++ fibo.py
@@ -1,6 +1,16 @@
+def recursive_nth_fibo(n):
+    """
+    snazime se najit konkretni cislo (poradi cisla) ve Fibonacciho rade
+    = soucet dvou predchozich cisel - 0, 1, 1, 2, 3, 5, 8 atd
+    """
+    if n <= 1:
+        return n
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
 
 def main():
-    pass
+    print(recursive_nth_fibo(30))
 
 
 if __name__ == "__main__":
